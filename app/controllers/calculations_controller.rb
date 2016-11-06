d0c030ea582262bf089fe8a3fe317b7815bfc68f
Kevin Marvinac <kevinmarvinac@users.noreply.github.com>
2016-11-06 12:06:23 -0600

    first effort

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,26 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @input = params[:id].to_i
+    render("square_root.html.erb")
+  end
+
+  def random
+    @start_num = params[:num_1].to_i + 1
+    @fin_num = params[:num_2].to_i
+    @answer = rand(@fin_num - @start_num) +@start_num
+    render("random.html.erb")
+  end
+
+  def payment
+    @periods = (params[:term].to_i)*12
+    @principal = params[:amount].to_i
+    @rate = (params[:rate].to_i)*(0.0001)
+    @payment = (((@rate/12) *@principal)/(1 - (1 + (@rate/12))**(-@periods)))
+    render("payment.html.erb")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:id", { :controller => "calculations", :action => "square" })
+
+  get("/random/:num_1/:num_2", { :controller => "calculations", :action => "random"})
+
+  get("/payment/:rate/:term/:amount", { :controller => "calculations", :action => "payment"})
+
 end
